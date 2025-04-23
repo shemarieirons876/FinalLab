08758f9c98d240aa4c3d5094395087cfea109a7c
Shemarie Irons <shemarieirons@gmail.com>
2025-04-23 10:03:21 +0000

    First commit

---
+++ mycipher.py
@@ -0,0 +1,26 @@
+import sys
+
+num = int(sys.argv[1]) % 26
+msg = sys.stdin.read()
+
+msg = msg.upper()
+final = ""
+count = 0
+
+for char in msg:
+
+    if not char.isalpha():
+        continue
+    count+=1
+    if (ord(char) + num) > 90:
+        final+=chr(ord(char)+num-26)
+    else:
+        final+=chr(ord(char)+num)
+        
+    if count%5 == 0:
+        final+=" "
+        
+    if count%50 == 0:
+        final+="\n"
+
+print (final)
